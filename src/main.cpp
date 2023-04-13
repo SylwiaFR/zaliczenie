f5488e0864cabb8f6fe050a56a77abeaeccb1be8
Sylwia Frackowiak <frackowiak.st@gmail.com>
2023-04-13 13:21:14 +0200

    zmiana w kodzie

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 
 int main () {
     double a,b;
-    cout << "Podaj dwie dowolne liczby od 1 do 9     " << endl;
+    cout << "Podaj dwie dowolne liczby z przedziału od 1 do 9     " << endl;
     cin >> a >> b;
     cout << liczby (a,b) << endl;
 
